129f4c6e0096a073d888148302ac25510ff6d9a6
savewinkie <Link.bernath5@gmail.com>
2026-04-29 20:23:27 +0200

    swap github and pricing

--- app/page.js
+++ app/page.js
@@ -36,8 +36,8 @@ export default function Home() {
         <Skills />
         <Projects />
         <Workflow />
-        <Pricing />
         <GithubStats />
+        <Pricing />
         <Contact />
         <Newsletter />
       </main>
